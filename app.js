dc8fd95c8180ae945a7d6a492b5d2b19d176320a
bestie06 <fatih.kgokmen@gmail.com>
2020-12-25 17:56:36 -0500

    GET a psecific article

--- app.js
+++ app.js
@@ -73,6 +73,21 @@ app.route("/articles").get(function(req,res){
 });
 
 
+app.route("/articles/:articleTitle")
+
+.get(function(req,res){
+
+    
+    Article.findOne({title:req.params.articleTitle}, function(err,foundArticle){
+        if(foundArticle){
+            res.send(foundArticle);
+
+        }else{
+            res.send("No articles matching that title");
+
+        }
+    });
+});
 
 
 app.listen(3000, function() {
