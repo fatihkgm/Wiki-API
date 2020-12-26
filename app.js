66d4936975583fbe12bd8b61c745d9503ee1eac2
bestie06 <fatih.kgokmen@gmail.com>
2020-12-25 20:21:35 -0500

    Patch method intested

--- app.js
+++ app.js
@@ -101,7 +101,22 @@ app.route("/articles/:articleTitle")
 
         }
     });
-});
+})
+.patch(function(req,res){
+
+    Article.update({title:req.params.articleTitle},
+        {$set: req.body},
+        function(err){
+            if(!err){
+                res.send("Succesfully updated")
+            }else{
+                res.send(err)
+            }
+        }
+        )
+
+
+})
 
 
 app.listen(3000, function() {
